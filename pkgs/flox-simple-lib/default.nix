12823a67be2003009b47fdf6a9e1307abb988d37
Damian Baar <damian@cloudprizm.io>
2023-10-02 20:05:30 +0300

    enabling flox

---
+++ pkgs/flox-simple-lib/default.nix
@@ -0,0 +1,16 @@
+{
+  self,
+  python3Packages,
+}:
+python3Packages.buildPythonPackage {
+  pname = "flox-simple-lib";
+  version = "0.0.0";
+  src = self;
+  PIP_DISABLE_PIP_VERSION_CHECK = 1;
+  # Add Python modules needed by your package here
+  propagatedBuildInputs = with python3Packages; [
+    requests
+    boto3
+  ];
+  meta.description = "an example flox package";
+}
