134f5ed08e93237689092b287f280ff5f6862390
Damian Baar <damian@cloudprizm.io>
2023-10-02 20:06:58 +0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flox-simple-lib",
+    version="0.4",
+    packages=find_packages(),
+    # entry_points={
+    #     'console_scripts': [
+    #         'flox-simple-project = hello_world:hello',
+    #     ],
+    # }
+)
