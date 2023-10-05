1edf1986c126e3a90bf669ef45b9d665feb5adcc
Damian Baar <damian@cloudprizm.io>
2023-10-05 12:22:48 +0200

    ..

--- pkgs/flox-simple-lib/default.nix
+++ pkgs/flox-simple-lib/default.nix
@@ -1,16 +1,29 @@
+# Replace "stdenv" with the namespace or name of your language's builder
 {
   self,
-  python3Packages,
+  stdenv,
+  lib,
 }:
-python3Packages.buildPythonPackage {
+# Replace "stdenv.mkDerivation" with your language's builder
+stdenv.mkDerivation {
   pname = "flox-simple-lib";
-  version = "0.0.1";
-  src = self;
-  PIP_DISABLE_PIP_VERSION_CHECK = 1;
-  # Add Python modules needed by your package here
-  propagatedBuildInputs = with python3Packages; [
-    requests
-    boto3
-  ];
+  version = "0.0.1-${lib.flox-floxpkgs.getRev self}";
+  src = self; # + "/src";
+
+  # Add runtime dependencies to buildInputs.
+  buildInputs = [];
+
+  # Add runtime dependencies required by packages that
+  # depend on this package to propagatedBuildInputs.
+  propagatedBuildInputs = [];
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r lib $out
+  '';
+
+  # Add buildtime dependencies (not required at runtime)
+  # to nativeBuildInputs.
+  nativeBuildInputs = [];
   meta.description = "an example flox package";
 }
